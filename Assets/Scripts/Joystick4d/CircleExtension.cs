829553aef8eb700f4770e4ee9b3e1647e6f57bf5
Mati <mbatista@ikatu.com>
2019-08-03 07:55:39 -0700

    Added functional 4-side fixed joystick controller for android

---
+++ Assets/Scripts/Joystick4d/CircleExtension.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class CircleExtension {
+    public static void DrawCircle(this GameObject gameObject, float radius, float lineWidth) {
+        var segments = 360;
+        var line = gameObject.GetComponent<LineRenderer>();
+        if (line == null) return;
+        line.useWorldSpace = false;
+        line.startWidth = lineWidth;
+        line.endWidth = lineWidth;
+        line.positionCount = segments + 1;
+        var pointCount = segments + 1;
+        var points = new Vector3[pointCount];
+        for (int pointIndex = 0; pointIndex < pointCount; pointIndex++) {
+            var rad = Mathf.Deg2Rad * (pointIndex * 360f / segments);
+            points[pointIndex] = new Vector3(Mathf.Sin(rad) * radius, 0, Mathf.Cos(rad) * radius);
+        }
+        line.SetPositions(points);
+    }
+}
